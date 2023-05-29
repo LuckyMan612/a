503f3dbab3909928b360adc06fe33bde22f800eb
LuckyMan612 <75744132+LuckyMan612@users.noreply.github.com>
2023-05-29 09:49:37 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// animations.js
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var elements = document.querySelectorAll(".animate__animated");
+
+    function animateElements() {
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            var delay = i * 200;
+            element.style.animationDelay = delay + "ms";
+            element.classList.add("animate__fadeIn");
+        }
+    }
+
+    animateElements();
+});
