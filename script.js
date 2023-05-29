20d86a6799e7f09c4c7290a1b72956cad5f59205
LuckyMan612 <75744132+LuckyMan612@users.noreply.github.com>
2023-05-29 10:55:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
     var elements = document.querySelectorAll(".animate__animated");
-    var modelContainer = document.getElementById("model-container");
-    var scene, camera, renderer, model;
 
     function animateElements() {
         for (var i = 0; i < elements.length; i++) {
@@ -14,31 +12,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
     }
 
-    function init() {
-        scene = new THREE.Scene();
-
-        camera = new THREE.PerspectiveCamera(75, modelContainer.clientWidth / modelContainer.clientHeight, 0.1, 1000);
-        camera.position.z = 5;
-
-        renderer = new THREE.WebGLRenderer();
-        renderer.setSize(modelContainer.clientWidth, modelContainer.clientHeight);
-        modelContainer.appendChild(renderer.domElement);
-
-        var loader = new THREE.GLTFLoader();
-        loader.load("ecodan_heat_pump_slim_02.obj", function(gltf) {
-            model = gltf.scene;
-            scene.add(model);
-
-            animate();
-        });
-    }
-
-    function animate() {
-        requestAnimationFrame(animate);
-        model.rotation.y += 0.01;
-        renderer.render(scene, camera);
-    }
-
     animateElements();
-    init();
 });
